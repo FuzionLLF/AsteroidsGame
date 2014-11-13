de40f51f31a12ea091fc6ec5a5d2ca8360afe5de
FuzionLLF <Fuzion117@gmail.com>
2014-11-13 11:26:02 -0800

    chris.html

--- build-tmp/source/AsteroidsGame.java
+++ build-tmp/source/AsteroidsGame.java
@@ -1,3 +1,19 @@
+import processing.core.*; 
+import processing.data.*; 
+import processing.event.*; 
+import processing.opengl.*; 
+
+import java.util.HashMap; 
+import java.util.ArrayList; 
+import java.io.File; 
+import java.io.BufferedReader; 
+import java.io.PrintWriter; 
+import java.io.InputStream; 
+import java.io.OutputStream; 
+import java.io.IOException; 
+
+public class AsteroidsGame extends PApplet {
+
 SpaceShip milleniumFalcon;
 Star [] group;//your variable declarations here
 public void setup() 
@@ -180,4 +196,13 @@ abstract class Floater //Do NOT modify the Floater class! Make changes in the Sp
     }   
     endShape(CLOSE);  
   }   
+}
+  static public void main(String[] passedArgs) {
+    String[] appletArgs = new String[] { "AsteroidsGame" };
+    if (passedArgs != null) {
+      PApplet.main(concat(appletArgs, passedArgs));
+    } else {
+      PApplet.main(appletArgs);
+    }
+  }
 }
